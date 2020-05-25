2d594ebb84ef49827e3004580a9639640ec83042
jatin _mehta <jatinmehta445@gmail.com>
2020-05-25 17:52:52 +0530

    Add project files.

---
+++ PDFManipulations/Controllers/MergePDFController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PDFManipulations.Models;
+using iTextSharp.text.pdf;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+using static System.Net.Mime.MediaTypeNames;
+using iTextSharp.text;
+
+namespace PDFManipulations.Controllers
+{
+    public class MergePDFController : Controller
+    {
+        private string[] filesPath = { "C\\PDF\\File1.pdf", "C\\PDF\\File2.pdf" };
+        public string outPutFilePath = "C\\PDF";
+        
+
+        [HttpGet]
+        [Route("MergePDFController")]
+        public IActionResult MergeFiles()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("MergePDFController/MergeFiless")]
+        public void MergeFiless()
+        {
+            List<PdfReader> readerList = new List<PdfReader>();
+            foreach (string filePath in filesPath)
+            {
+                PdfReader pdfReader = new PdfReader(filePath);
+                readerList.Add(pdfReader);
+            }
+            Document document = new Document(PageSize.A4, 0, 0, 0, 0);
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(outPutFilePath, FileMode.Create));
+            document.Open();
+            foreach (PdfReader reader in readerList)
+            {
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    PdfImportedPage page = writer.GetImportedPage(reader, i);
+                    document.Add(iTextSharp.text.Image.GetInstance(page));
+                }
+            }
+            document.Close();
+        }
+
+
+    }
+}
---
+++ PDFManipulations/Models/EmpModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace PDFManipulations.Models
+{
+    public class EmpModel
+    {
+        [Required]
+        [DataType(DataType.Upload)]
+        [Display(Name = "Select File")]
+        public IFormFile files { get; set; }
+    }
+
+    public class FileDetailsModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Uploaded File")]
+        public String FileName { get; set; }
+        public byte[] FileContent { get; set; }
+
+
+    }
+}
